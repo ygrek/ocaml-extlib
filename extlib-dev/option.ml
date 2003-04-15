06e4875b6ed32b4b0cc9afe41fafe209d5224984
ncannasse <ncannasse@gmail.com>
2003-04-15 11:32:13 +0000

    added implementation.

---
+++ extlib-dev/option.ml
@@ -0,0 +1,25 @@
+exception ValNone
+
+let may f = function
+	| None -> ()
+	| Some v -> f v
+
+let map f = function
+	| None -> None
+	| Some v -> Some (f v)
+
+let default v = function
+	| None -> v
+	| Some v -> v
+
+let is_some = function
+	| None -> false
+	| _ -> true
+
+let is_none = function
+	| None -> true
+	| _ -> false
+
+let get = function
+	| None -> raise ValNone
+	| Some v -> v
--- extlib-dev/option.mli
+++ extlib-dev/option.mli
@@ -5,4 +5,4 @@ val map : ('a -> 'b) -> 'a option -> 'b option
 val default : 'a -> 'a option -> 'a
 val is_some : 'a option -> bool
 val is_none : 'a option -> bool
-val val_of : 'a option -> 'a (* raise ValNone *)
+val get : 'a option -> 'a (* raise ValNone *)
