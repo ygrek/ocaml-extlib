5eb37e0e0ad64dcec842250dcf63027b21e8ef88
ncannasse <ncannasse@gmail.com>
2003-04-22 03:22:35 +0000

    minor changes.

--- extlib-dev/extList.ml
+++ extlib-dev/extList.ml
@@ -16,7 +16,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * 03-15-03 : Modified by Nicolas Cannasse , added functions
  *)
 
 module List = struct
