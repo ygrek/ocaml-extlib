b33c54b257dd477abb5a867881de4d9856da1532
ncannasse <ncannasse@gmail.com>
2003-05-16 11:27:34 +0000

    added comments.

--- extlib-dev/extLib.ml
+++ extlib-dev/extLib.ml
@@ -1,3 +1,17 @@
+(* Module ExtLib
+	(c)2003 Nicolas Cannasse
+
+	Note:
+	
+	Since ExtLib is provided for namespace convenience for
+	users who wants to keep the usage of the original
+	Ocaml Standard Library, no CMI nor documentation will
+	be provided for this module.
+
+	Users can simply do an "open ExtLib" to import all Ext*
+	namespaces instead of doing "open ExtList" for example.
+*)
+
 module ExtList = ExtList.List
 module ExtArray = ExtArray.Array
 module ExtString = ExtString.String
