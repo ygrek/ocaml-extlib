c71a406095f7ae58812d7d4bb5eeee9e441de4f4
ncannasse <ncannasse@gmail.com>
2003-05-23 02:45:30 +0000

    changed clone implementation.

--- extlib-dev/enum.ml
+++ extlib-dev/enum.ml
@@ -244,44 +244,36 @@ let filter_map f t =
     from next
 
 let clone t =
+	let cache_t = ref [] in
+	let cache_t_count = ref 0 in
+	let cache_tc = ref [] in
+	let cache_tc_count = ref 0 in
 	let fnext = t.next in
+	let fcount = t.count in
 	let tc = {
-		next = _dummy;
-		count = t.count;
+		next = (fun () ->
+					match !cache_tc with
+					| [] ->
+						let e = fnext() in
+						incr cache_t_count;
+						cache_t := e :: !cache_t;
+						e
+					| h :: t ->
+						decr cache_tc_count;
+						cache_tc := t;
+						h);
+		count = (fun () -> fcount() + !cache_tc_count);
 	} in
-	let force2 () =
-		let count = ref 1 in
-		let rec loop dst =
-			let x = [fnext()] in
-			incr count;
-			Obj.set_field (Obj.repr dst) 1 (Obj.repr x);
-			loop x
-		in
-		try
-			let x = [fnext()] in
-			(try loop x with No_more_elements -> ());
-			let enum = ref x in
-			t.count <- (fun () -> !count);
-			t.next <- (fun () ->
-				decr count;
-				match !enum with
-				| [] -> raise No_more_elements
-				| h :: t -> enum := t; h);
-			let count2 = ref !count in
-			let enum2 = ref !enum in
-			tc.count <- (fun () -> !count2);
-			tc.next <- (fun () ->
-				decr count2;
-				match !enum2 with
-				| [] -> raise No_more_elements
-				| h :: t -> enum2 := t; h);
-		with
-			No_more_elements ->
-				t.count <- (fun () -> 0);
-				tc.count <- (fun () -> 0);
-				t.next <- (fun () -> raise No_more_elements);
-				tc.next <- (fun () -> raise No_more_elements)
-	in
-	t.next <- (fun () -> force2(); t.next());
-	tc.next <- (fun () -> force2(); tc.next());
+	t.next <- (fun () ->
+		match !cache_t with
+		| [] ->
+			let e = fnext() in
+			incr cache_tc_count;
+			cache_tc := e :: !cache_tc;
+			e
+		| h :: t ->
+			decr cache_t_count;
+			cache_t := t;
+			h);
+	t.count <- (fun () -> fcount() + !cache_t_count);
 	tc
