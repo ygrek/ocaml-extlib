044ab8eff31418b17441d19cf8969fde34ee2bb6
bhurt <brian.hurt@qlogic.com>
2003-05-25 02:58:02 +0000

    De-funtorized things.  Also fixed up queue_to_string to use a string buffer instead of just endlessly concating strings- this should be more efficient.

--- extlib-dev/psqueue.mli
+++ extlib-dev/psqueue.mli
@@ -35,166 +35,117 @@
     implementation uses Red-Black trees as it's balancing algorithm.
 *)
 
+type ('a, 'b) t (** The type of priority search queues *)
 
-module type OrderedPriorityType =
-    sig
+exception Empty
+(** Thrown when a head, find, precise adjust, or precise deletion is
+    attempted on an empty queue. *)
 
-        type k_t
-            (** The type of keys *)
+exception Exists
+(** Thrown when a precise insertion is attempted with a key that
+    already exists in the queue. *)
 
-        type e_t
-            (** The type of elements.  We assume priorities are some innate
-                property of elements, so priority compares take two elements.
-                I consider this more efficient than having a seperate priority
-                type. *)
+val create : ('a -> 'a -> int) -> ('b -> 'b -> int) -> ('a, 'b) t
 
-        val pricmp : e_t -> e_t -> int
-            (** Priority compare of elements.
-                This is a two-argument function [f] such that
-                [f e1 e2] is zero if elements [e1] and [e2] have the same
-                priority, [f e1 e2] is strictly negative if [e1] has a
-                higher priority than [e2], and [f e1 e2] is strictly
-                positive if [e1] has a lower priority then [e2].
-                Note that if priorities are ints, then the lesser
-                number has a higher priority!  This makes sense- which
-                is higher priority, priority 1 items, or priority 2 items? *)
+val head : ('a, 'b) t -> 'b
+(** Returns the head (highest priority) element of the queue.
+    This is an O(1) operation.
+    @raise Empty if the queue is empty. *)
 
-        val keycmp : k_t -> k_t -> int
-            (** Ordering of two keys.
-                This is a two-argument function [f] such that
-                [f k1 k2] is zero if key [k1] and [k2] are unordered (i.e.
-                are equal), [f k1 k2] is strictly negative if [k1] is
-                before [k2], and strictly positive if [k1] is after [k2].
-                A suitable ordering function is the generic structural
-                comparison [compare]. *)
-    end
-(** Input signature of the functor {!Psqueue.Make}. *)
+val head_key : ('a, 'b) t -> 'a
 
-module type PSQ = 
-    sig
+val is_empty : ('a, 'b) t -> bool
+(** Returns true if the queue is empty.  This is an O(1) operation. *)
 
-        type key_t
-        (** The type of the search key *)
+val to_ord_list : ('a, 'b) t -> ('a * 'b) list
+(** Returns all elements of the queue in sorted order by increasing
+    key.  This is an O(n) operation. *)
 
-        type elem_t
-        (** The type of the elements *)
+val to_pri_list : ('a, 'b) t -> ('a * 'b) list
+(** Returns all elements of the queue in sorted order by decreasing
+    priority.  This is an O(n log n) operation. *)
 
-        exception Empty
-        (** Thrown when a head, find, precise adjust, or precise deletion is
-            attempted on an empty queue. *)
+(* Precise operations- these operations throw exceptions on
+ * unexpected cases.
+ *)
 
-        exception Exists
-        (** Thrown when a precise insertion is attempted with a key that
-            already exists in the queue. *)
+val find : 'a -> ('a, 'b) t -> 'b
+(** Precise search.
+    [find x q] returns the current binding of [x] in [q].
+    @raise Not_found if no such binding exists. *)
 
-        type psq_t (* abstract *)
-        (** The type of priority search queues *)
+val adjust : ('b -> 'b) -> 'a -> ('a, 'b) t -> ('a, 'b) t
+(** Precise priority adjustment.
+    [adjust f k q] changes the priority of the element bound to
+    [k] in [q] by applying [f] to it.
+    @raise Not_found if no such binding exists
+    @raise Empty if [q] is empty (contains no bindings). *)
 
-        val empty : psq_t
-        (** The empty queue *)
+val add : 'a -> 'b -> ('a, 'b) t -> ('a, 'b) t
+(** Precise insertion.
+    [add k e q] returns a queue containing the same bindings as [q]
+    plus a binding of [k] to [e].
+    @raise Exists if a binding for [k] already exists *)
 
-        val head : psq_t -> elem_t
-        (** Returns the head (highest priority) element of the queue.
-            This is an O(1) operation.
-            @raise Empty if the queue is empty. *)
+val remove : 'a -> ('a, 'b) t -> ('a, 'b) t
+(** Precise deletion.
+    [remove k q] returns a queue containing the same bindings as [q]
+    except for the binding from [k], which is unbound in the returned
+    queue.
+    @raise Not_found if the key does not have a binding in the given 
+    queue
+    @raise Empty if the given queue is empty. *)
 
-        val is_empty : psq_t -> bool
-        (** Returns true if the queue is empty.  This is an O(1) operation. *)
+val remove_head : ('a, 'b) t -> ('a, 'b) t
+(** Precise head removal.
+    [remove_head q] returns a queue with the same mappings as [q]
+    except that the mapping with the highest priority element is
+    unbound (removed).
+    @raise Empty if the given queue is empty. *)
 
-        val to_ord_list : psq_t -> (key_t * elem_t) list
-        (** Returns all elements of the queue in sorted order by increasing
-            key.  This is an O(n) operation. *)
+(* Imprecise operations- these "nice" operations don't raise
+ * exceptions, they just do something more or less intelligent
+ * in unexpected situations.
+ *)
 
-        val to_pri_list : psq_t -> (key_t * elem_t) list
-        (** Returns all elements of the queue in sorted order by decreasing
-            priority.  This is an O(n log n) operation. *)
+val contains : 'a -> ('a, 'b) t -> bool
+(** [contains x q] evaluates to true if a binding for [x] exists in
+    [q], false otherwise. *)
 
-        (* Precise operations- these operations throw exceptions on
-         * unexpected cases.
-         *)
+val query : 'b -> 'a -> ('a, 'b) t -> 'b
+(** Imprecise search.
+    [query d k q] returns the current binding of [k] in [q], or
+    [d] if no such binding exists.  This is different from [find]
+    in its behavior when the key does not exist in the queue. *)
 
-        val find : key_t -> psq_t -> elem_t
-        (** Precise search.
-            [find x q] returns the current binding of [x] in [q].
-            @raise Not_found if no such binding exists. *)
+val update : ('b -> 'b) -> 'a -> ('a, 'b) t -> ('a, 'b) t
+(** Imprecise priority adjustment.
+    [update f k q] changes the priority of the element bound to
+    [k] in [q] by applying [f] to it.  If [k] is not bound in
+    [q], nothing happens. *)
 
-        val adjust : (elem_t -> elem_t) -> key_t -> psq_t -> psq_t
-        (** Precise priority adjustment.
-            [adjust f k q] changes the priority of the element bound to
-            [k] in [q] by applying [f] to it.
-            @raise Not_found if no such binding exists
-            @raise Empty if [q] is empty (contains no bindings). *)
+val insert : 'a -> 'b -> ('a, 'b) t -> ('a, 'b) t
+(** Imprecise insertion.
+    [insert k e q] returns a queue containing the same bindings as [q]
+    plus a binding of [k] to [e].  If [k] is already bound in [q],
+    its previous binding is replaced by the new binding. *)
 
-        val add : key_t -> elem_t -> psq_t -> psq_t
-        (** Precise insertion.
-            [add k e q] returns a queue containing the same bindings as [q]
-            plus a binding of [k] to [e].
-            @raise Exists if a binding for [k] already exists *)
+val delete : 'a ->  ('a, 'b) t ->  ('a, 'b) t
+(** Imprecise deletion.
+    [remove k q] returns a queue containing the same bindings as [q]
+    except for the binding from [k], which is unbound in the returned
+    queue.  If [k] is not bound in [q], the returned queue is the
+    same as [q]. *)
 
-        val remove : key_t -> psq_t -> psq_t
-        (** Precise deletion.
-            [remove k q] returns a queue containing the same bindings as [q]
-            except for the binding from [k], which is unbound in the returned
-            queue.
-            @raise Not_found if the key does not have a binding in the given 
-            queue
-            @raise Empty if the given queue is empty. *)
+val delete_head : ('a, 'b) t -> ('a, 'b) t
+(** Imprecise head removal.
+    [delete_head q] returns a queue with the same mappings as [q]
+    except that the mapping with the highest priority element is
+    unbound (removed).  If [q] is empty, the empty queue is returned.
+    *)
 
-        val remove_head : psq_t -> psq_t
-        (** Precise head removal.
-            [remove_head q] returns a queue with the same mappings as [q]
-            except that the mapping with the highest priority element is
-            unbound (removed).
-            @raise Empty if the given queue is empty. *)
-
-        (* Imprecise operations- these "nice" operations don't raise
-         * exceptions, they just do something more or less intelligent
-         * in unexpected situations.
-         *)
-
-        val contains : key_t -> psq_t -> bool
-        (** [contains x q] evaluates to true if a binding for [x] exists in
-            [q], false otherwise. *)
-
-        val query : elem_t -> key_t -> psq_t -> elem_t
-        (** Imprecise search.
-            [query d k q] returns the current binding of [k] in [q], or
-            [d] if no such binding exists.  This is different from [find]
-            in its behavior when the key does not exist in the queue. *)
-
-        val update : (elem_t -> elem_t) -> key_t -> psq_t -> psq_t
-        (** Imprecise priority adjustment.
-            [update f k q] changes the priority of the element bound to
-            [k] in [q] by applying [f] to it.  If [k] is not bound in
-            [q], nothing happens. *)
-
-        val insert : key_t -> elem_t -> psq_t -> psq_t
-        (** Imprecise insertion.
-            [insert k e q] returns a queue containing the same bindings as [q]
-            plus a binding of [k] to [e].  If [k] is already bound in [q],
-            its previous binding is replaced by the new binding. *)
-
-        val delete : key_t ->  psq_t ->  psq_t
-        (** Imprecise deletion.
-            [remove k q] returns a queue containing the same bindings as [q]
-            except for the binding from [k], which is unbound in the returned
-            queue.  If [k] is not bound in [q], the returned queue is the
-            same as [q]. *)
-
-        val delete_head : psq_t -> psq_t
-        (** Imprecise head removal.
-            [delete_head q] returns a queue with the same mappings as [q]
-            except that the mapping with the highest priority element is
-            unbound (removed).  If [q] is empty, the empty queue is returned.
-            *)
-
-        val queue_to_string : (key_t -> string) -> (elem_t -> string) -> psq_t -> string
-        (** Creates a string representation of the queue (for I/O).
-            This function is horribly inefficient.  Only use it for debugging.
-         *)
-
-    end
-(** Output signature of the function {!Psqueue.Make}. *)
-
-module Make (Ord: OrderedPriorityType) : PSQ with type key_t = Ord.k_t and type elem_t = Ord.e_t
+val queue_to_string : ('a -> string) -> ('b -> string) -> ('a, 'b) t -> string
+(** Creates a string representation of the queue (for I/O).
+    This function is horribly inefficient.  Only use it for debugging.
+ *)
 
