bc106021114701baa720db5c0a70438d4fedcda6
yoriyuki <yoriyuki.y@gmail.com>
2003-07-03 16:10:26 +0000

    LGPL preambles, ocamldoc style comments.

--- extlib-dev/uChar.mli
+++ extlib-dev/uChar.mli
@@ -1,48 +1,73 @@
-(* Copyright 2002, 2003 Yamagata Yoriyuki *)
+(* 
+ * UChar - Unicode (ISO-UCS) characters
+ * Copyright (C) 2002, 2003 Yamagata Yoriyuki
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
 
-(* Unicode Characters. All 31bit code points are supported.*)
+(** Unicode (ISO-UCS) characters.
+
+   This module implements Unicode (actually ISO-UCS) characters.  All
+   31-bit code points are allowed.
+*)
+
+(** Unicode characters. All 31bit code points are allowed.*) 
 type t
 
 exception Out_of_range
 
-(* char u : returns the Latin-1 representation of [u].
- * If [u] can not be represented by Latin-1, raises Out_of_range *)
+(** [char_of u] returns the Latin-1 representation of [u].
+   If [u] can not be represented by Latin-1, raises Out_of_range *)
 val char_of : t -> char
 
-(* char c : returns the Unicode character of the Latin-1 character [c] *)
+(** [of_char c] returns the Unicode character of the Latin-1 character [c] *)
 val of_char : char -> t
 
-(* code u : returns the Unicode code number of [u].
- * If the value can not be represented by a positive integer,
- * raise Out_of_range *)
+(** [code u] returns the Unicode code number of [u].
+   If the value can not be represented by a positive integer,
+   raise Out_of_range *)
 val code : t -> int
 
-(* code n : returns the Unicode character with the code number [n].
- * If n exceeded 31-bit value, raises invalid_arg *)
+(** [code n] returns the Unicode character with the code number [n]. 
+   If n exceeded 31-bit value, raises invalid_arg *)
 val chr : int -> t
 
-(* uint_code u : returns the Unicode code number of [u].
- * The returned int is unsigned, that is, on 32-bits platforms,
- * the sign bit is used for storing the 31-th bit of the code number. *)
+(** [uint_code u] returns the Unicode code number of [u].
+   The returned int is unsigned, that is, on 32-bits platforms,
+   the sign bit is used for storing the 31-th bit of the code number. *)
 val uint_code : t -> int
 
-(* chr_of_uint n : returns the Unicode character of the code number [n].
- * [n] is interpreted as unsigned, that is, on 32-bits platforms,
- * the sign bit is treated as the 31-th bit of the code number. *)
+(** [chr_of_uint n] returns the Unicode character of the code number [n].
+   [n] is interpreted as unsigned, that is, on 32-bits platforms,
+   the sign bit is treated as the 31-th bit of the code number. *)
 val chr_of_uint : int -> t
 
-(* Code point comparison. *)
-(* Equality *)
+(** Equality by code point comparison *)
 val eq : t -> t -> bool
-(* compare u1 u2 : returns, 
- * if [u1] has a larger Unicode code number than [u2], a value > 0,
- * if [u1] and [u2] are the same Unicode character, 0,
- * if [u1] has a smaller Unicode code number than [u2], a value < 0 *)
+
+(** [compare u1 u2] returns, 
+   a value > 0 if [u1] has a larger Unicode code number than [u2], 
+   0 if [u1] and [u2] are the same Unicode character,
+   a value < 0 if [u1] has a smaller Unicode code number than [u2]. *)
 val compare : t -> t -> int
 
-(* Aliases *)
+(** Aliases of [type t] *)
 type uchar = t
-(* Alias of uint_code *)
+
+(** Alias of [uint_code] *)
 val int_of_uchar : uchar -> int
-(* Alias of chr_of_uint *)
+
+(** Alias of [chr_of_uint] *)
 val uchar_of_int : int -> uchar
