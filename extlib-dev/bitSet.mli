4e67070bb6c92e524593631509d15e623e149262
ncannasse <ncannasse@gmail.com>
2003-09-19 15:14:43 +0000

    added documentation.

--- extlib-dev/bitSet.mli
+++ extlib-dev/bitSet.mli
@@ -1,6 +1,6 @@
 (*
  * Bitset - Efficient bit sets
- * Copyright (C) 2003 Nicolas Cannasse (ncannasse@motion-twin.com)
+ * Copyright (C) 2003 Nicolas Cannasse
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,22 +17,55 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
-exception Negative_index of string
+(** Efficient bit sets.
+
+ A bitset is a array of boolean values that can be accessed with indexes
+ like an array but provide a better memory usage (divided by 32) for a
+ very small speed tradeoff. *)
 
 type t
 
+exception Negative_index of string
+(** When a negative bit value is used for one of the BitSet functions,
+ this exception is raised with the name of the function. *)
+
 val empty : unit ->  t
+(** Create an empty bitset of size 0, the bitset will automaticaly expands
+ when needed. *)
+
 val create : int -> t
+(** Create an empty bitset with an initial size (in number of bits). *)
+
 val clone : t -> t
+(** Clone a bitset : further modifications of first one will not affect the
+ clone. *)
 
 val set : t -> int -> unit
+(** [set s n] set the nth-bit in the bitset [s] to true. *)
+ 
 val unset : t -> int -> unit
+(** [unset s n] set the nth-bit in the bitset [s] to false. *) 
+
+val put : t -> bool -> int -> unit
+(** [put s v n] set the nth-bit in the bitset [s] to [v]. *)
+
 val toggle : t -> int -> unit
+(** [toggle s n] change the nth-bit value in the bitset [s]. *)
 
 val is_set : t -> int -> bool
+(** [is_set s n] return true if nth-bit it the bitset [s] is set,
+ or false otherwise. *)
 
 val compare : t -> t -> int
+(** [compare s1 s2] compare two bitsets. Highest bit indexes are
+ compared first. *)
+
 val equals : t -> t -> bool
+(** [equals s1 s2] return true if all bits value in s1 are same as s2. *)
 
 val count : t -> int
+(** [count s] returns the number of bits set in the bitset [s]. *)
+
 val enum : t -> int Enum.t
+(** [enum s] return an enumeration of bit indexed which are set
+ in the bitset [s]. *)
