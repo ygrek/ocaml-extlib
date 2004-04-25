f6990286f620c3227bcaab4ca8b9b6a27013b712
ncannasse <ncannasse@gmail.com>
2004-04-25 09:01:53 +0000

    added IO codec.

--- extlib-dev/base64.ml
+++ extlib-dev/base64.ml
@@ -16,42 +16,95 @@ let inv_chars =
 	done;
 	a
 
-let encode s =
+let encode ch =
 	let data = ref 0 in
 	let count = ref 0 in
-	let b = Buffer.create 0 in
-	let l = String.length s - 1 in
-	for i = 0 to l do
-		let c = int_of_char (String.unsafe_get s i) in
+	let flush() =
+		if !count > 0 then begin
+			let d = (!data lsl (6 - !count)) land 63 in
+			IO.write ch (Array.unsafe_get chars d);
+		end;		
+	in
+	let write c =
+		let c = int_of_char c in
 		data := (!data lsl 8) lor c;
 		count := !count + 8;
 		while !count >= 6 do
 			count := !count - 6;
 			let d = (!data asr !count) land 63 in
-			Buffer.add_char b (Array.unsafe_get chars d)
+			IO.write ch (Array.unsafe_get chars d)
 		done;
-	done;
-	if !count > 0 then begin
-		let d = (!data lsl (6 - !count)) land 63 in
-		Buffer.add_char b (Array.unsafe_get chars d);
-	end;
-	Buffer.contents b
+	in
+	let nwrite s =
+		let l = String.length s - 1 in
+		for i = 0 to l do
+			write (String.unsafe_get s i)
+		done;
+	in
+	let pos() =
+		let p = IO.pos_out ch in
+		if p = -1 then
+			-1
+		else
+			((p * 6) + !count) / 8
+	in
+	IO.create_out ~write ~nwrite ~pos
+		~flush:(fun () -> flush(); IO.flush ch)
+		~close:(fun() -> flush(); IO.close_out ch)
 
-let decode s =
+let decode ch =
 	let data = ref 0 in
 	let count = ref 0 in
-	let b = Buffer.create 0 in
-	let l = String.length s - 1 in
-	for i = 0 to l do
-		let c = int_of_char (String.unsafe_get s i) in
-		let c = Array.unsafe_get inv_chars c in
-		if c = -1 then raise Invalid_char;
-		data := (!data lsl 6) lor c;
-		count := !count + 6;
-		while !count >= 8 do
+	let rec fetch() =
+		if !count >= 8 then begin
 			count := !count - 8;
 			let d = (!data asr !count) land 0xFF in
-			Buffer.add_char b (unsafe_char_of_int d);
-		done;
-	done;
-	Buffer.contents b
+			unsafe_char_of_int d
+		end else
+			let c = int_of_char (IO.read ch) in
+			let c = Array.unsafe_get inv_chars c in
+			if c = -1 then raise Invalid_char;
+			data := (!data lsl 6) lor c;
+			count := !count + 6;
+			fetch()
+	in
+	let read = fetch in
+	let nread n =
+		let s = String.create n in
+		let i = ref 0 in
+		try
+			while !i < n do
+				String.unsafe_set s !i (fetch());
+				incr i;
+			done;
+			s
+		with
+			IO.No_more_input when !i > 0 ->
+				String.sub s 0 !i
+	in
+	let pos() =
+		let p = IO.pos_in ch in
+		if p = -1 then
+			-1 
+		else
+			((p * 6) - !count) / 8
+	in
+	let available() =
+		match IO.available ch with
+		| None -> None
+		| Some n -> Some (((n * 6) + !count) / 8)
+	in
+	let close() =
+		count := 0;
+		IO.close_in ch
+	in
+	IO.create_in ~read ~nread ~pos ~available ~close
+
+let str_encode s =
+	let ch = encode (IO.output_string()) in
+	IO.nwrite ch s;
+	IO.close_out ch
+
+let str_decode s =
+	let ch = decode (IO.input_string s) in
+	IO.nread ch ((String.length s * 6) / 8)
--- extlib-dev/base64.mli
+++ extlib-dev/base64.mli
@@ -1,4 +1,40 @@
+(*
+ * Base64 - Base64 codec
+ * Copyright (C) 2003 Nicolas Cannasse
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
+
+(** Base64 codec.
+
+ 8-bits characters are encode into 6-bits ones using ASCII characters A-Z,
+ a-z, 0-9, and '+' , '/' (in that order). *)
+
+(** This exception is raised when reading an invalid Base64 character
+	from a base64 input. *)
 exception Invalid_char
 
-val encode : string -> string
-val decode : string -> string
+(** encode a string into Base64 *)
+val str_encode : string -> string
+
+(** decode a string encoded into Base64, raise [Invalid_char] if a
+	character in the input string is not a base64 one. *)
+val str_decode : string -> string
+
+(** Generic base64 encoding over an output. *)
+val encode : (char, 'a, 'b) IO.output -> (char, string, 'b) IO.output
+
+(** Generic base64 decoding over an input. *)
+val decode : (char, 'a) IO.input -> (char, string) IO.input
