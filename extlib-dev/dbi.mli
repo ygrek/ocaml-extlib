64cd17b3b2ba94655abdca299522ed2feb78d1cf
ncannasse <ncannasse@gmail.com>
2004-05-02 10:47:26 +0000

    fixed licenced (change approved by Richard Jones).

--- extlib-dev/dbi.mli
+++ extlib-dev/dbi.mli
@@ -4,7 +4,8 @@
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version,
+ * with the special exception on linking described in file LICENCE.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +16,7 @@
  * License along with this library; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * $Id: dbi.mli,v 1.1 2004-04-25 09:03:46 ncannasse Exp $
+ * $Id: dbi.mli,v 1.2 2004-05-02 10:47:26 ncannasse Exp $
  *)
 
 (** Generic database interface.
