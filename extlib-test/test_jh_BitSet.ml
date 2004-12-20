7831af1fc7e2372ab8abea47c6ef00273e8ddf3b
nurpax <jjhellst@gmail.com>
2004-12-20 20:38:50 +0000

    Added license notices to source files.  Cosmetic changes to test logging.

--- extlib-test/test_jh_BitSet.ml
+++ extlib-test/test_jh_BitSet.ml
@@ -1,3 +1,22 @@
+(*
+ * ExtLib Testing Suite
+ * Copyright (C) 2004 Janne Hellsten
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version,
+ * with the special exception on linking described in file LICENSE.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
 
 module B = BitSet
 
@@ -142,11 +161,11 @@ let test_set_opers () =
   done
 *)
 let test () =
-  Util.run_test ~test_name:"jh_BitSet2.test_intersect" test_intersect;
-  Util.run_test ~test_name:"jh_BitSet2.test_diff" test_diff;
-  Util.run_test ~test_name:"jh_BitSet2.test_rnd_creation" test_rnd_creation;
-  Util.run_test ~test_name:"jh_BitSet2.test_empty" test_empty;
-  Util.run_test ~test_name:"jh_BitSet2.test_exceptions" test_exceptions;
+  Util.run_test ~test_name:"jh_BitSet.test_intersect" test_intersect;
+  Util.run_test ~test_name:"jh_BitSet.test_diff" test_diff;
+  Util.run_test ~test_name:"jh_BitSet.test_rnd_creation" test_rnd_creation;
+  Util.run_test ~test_name:"jh_BitSet.test_empty" test_empty;
+  Util.run_test ~test_name:"jh_BitSet.test_exceptions" test_exceptions;
 (*  Util.run_test ~test_name:"jh_BitSet2.test_set_opers" test_set_opers*)
   
 
