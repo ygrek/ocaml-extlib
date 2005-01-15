6d271b2e4ea6d9ca110497cebdd52ff8e25bc866
nurpax <jjhellst@gmail.com>
2005-01-15 13:10:34 +0000

    Added new tests for BitSet differentiate and differentiate_sym

--- extlib-test/test_jh_BitSet_002.ml
+++ extlib-test/test_jh_BitSet_002.ml
@@ -96,7 +96,53 @@ let test_intersect () =
     assert (B.count c = 0);
   done
 
+let test_differentiate () = 
+  for i = 0 to 255 do
+    let r1 = biased_rnd_28 () in
+    let s = bitset_of_int r1 in
+    let d = B.copy s in
+    B.differentiate d s;
+    assert (B.count d = 0);
+    for j = 0 to 32 do
+      B.set s (Random.int 256)
+    done;
+    let d = B.copy s in
+    B.differentiate d (B.empty ());
+    assert (B.count s = B.count d);
+    assert (B.compare d s = 0);
+    B.differentiate d s;
+    assert (B.count d = 0);
+  done
+
+(* TODO *)
+let test_differentiate_sym () = 
+  for i = 0 to 255 do
+    let r1 = biased_rnd_28 () in
+    let r2 = biased_rnd_28 () in
+    let s = bitset_of_int r1 in
+    let d = B.copy s in
+    B.differentiate_sym d s;
+    assert (B.count d = 0);
+    for j = 0 to 32 do
+      B.set s (Random.int 256)
+    done;
+    let d = B.copy s in
+    B.differentiate_sym d (B.empty ());
+    assert (B.count s = B.count d);
+    assert (B.compare d s = 0);
+    B.differentiate_sym d s;
+    assert (B.count d = 0);
+
+    let s1 = bitset_of_int r1 
+    and s2 = bitset_of_int r2 in
+    let d1 = B.copy s1 in
+    B.differentiate_sym d1 s2;
+    assert (r1 lxor r2 = int_of_bitset d1);
+  done
 
 let test () =
   Util.run_test ~test_name:"jh_BitSet_002.test_unite" test_unite;
-  Util.run_test ~test_name:"jh_BitSet_002.test_intersect" test_intersect
+  Util.run_test ~test_name:"jh_BitSet_002.test_intersect" test_intersect;
+  Util.run_test ~test_name:"jh_BitSet_002.test_differentiate" test_differentiate;
+  Util.run_test ~test_name:"jh_BitSet_002.test_differentiate_sym" 
+    test_differentiate_sym;
