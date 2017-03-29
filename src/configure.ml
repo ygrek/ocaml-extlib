42c557d079216868b07806bebb5a1e34fa7eb73f
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-03-28 20:00:30 -0400

    src/configure.ml: add OCAML4_05

--- src/configure.ml
+++ src/configure.ml
@@ -3,5 +3,6 @@ let () =
   print_endline (if Sys.ocaml_version >= "4.02.0" then "-D OCAML4_02 " else "");
   print_endline (if Sys.ocaml_version >= "4.03.0" then "-D OCAML4_03 " else "");
   print_endline (if Sys.ocaml_version >= "4.04.0" then "-D OCAML4_04 " else "");
+  print_endline (if Sys.ocaml_version >= "4.05.0" then "-D OCAML4_05 " else "");
   let (_:int) = Sys.command "ocamlfind query -format \"-D WITH_BYTES\" bytes" in ();
   exit 0
