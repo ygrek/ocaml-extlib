2d858e036d18fcfa84de563580738659357e120a
ygrek <ygrek@autistici.org>
2017-07-14 21:50:55 -0700

    src/configure.ml: version check for -bin-annot

--- src/configure.ml
+++ src/configure.ml
@@ -1,8 +1,14 @@
 let () =
-  print_endline (if Sys.ocaml_version >= "4.00.0" then "-D OCAML4 " else "");
-  print_endline (if Sys.ocaml_version >= "4.02.0" then "-D OCAML4_02 " else "");
-  print_endline (if Sys.ocaml_version >= "4.03.0" then "-D OCAML4_03 " else "");
-  print_endline (if Sys.ocaml_version >= "4.04.0" then "-D OCAML4_04 " else "");
-  print_endline (if Sys.ocaml_version >= "4.05.0" then "-D OCAML4_05 " else "");
-  let (_:int) = Sys.command "ocamlfind query -format \"-D WITH_BYTES\" bytes" in ();
-  exit 0
+  match Sys.argv with
+  | [|_;"-cppo-args"|] ->
+    print_endline (if Sys.ocaml_version >= "4.00.0" then "-D OCAML4 " else "");
+    print_endline (if Sys.ocaml_version >= "4.02.0" then "-D OCAML4_02 " else "");
+    print_endline (if Sys.ocaml_version >= "4.03.0" then "-D OCAML4_03 " else "");
+    print_endline (if Sys.ocaml_version >= "4.04.0" then "-D OCAML4_04 " else "");
+    print_endline (if Sys.ocaml_version >= "4.05.0" then "-D OCAML4_05 " else "");
+    let (_:int) = Sys.command "ocamlfind query -format \"-D WITH_BYTES\" bytes" in ();
+    exit 0
+  | [|_;"-compile-args"|] ->
+    if Sys.ocaml_version >= "4.00.0" then print_endline "-bin-annot";
+    exit 0
+  | _ -> failwith "not gonna happen"
